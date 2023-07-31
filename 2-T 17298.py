414e69a8a9de0db59efde7048778f31e15fd2fab
sdj3261 <63071967+sdj3261@users.noreply.github.com>
2023-07-31 14:06:18 +0000

    2023-07-31 오큰수

--- 2-T 17298.py
+++ 2-T 17298.py
@@ -1,10 +1,21 @@
 from collections import deque
 n = int(input())
 arr = list(map(int,input().split()))
-ret = []
-tmp = []
-for idx in range(1,n) :
-    if arr[idx] > arr[idx-1] :
-        ret.append(arr[idx])
-    else 
+q = deque()
+ret = [-1] * n  # Initialize the ret list with -1
+
+
+answer = [-1] * n
+stack = []
+
+
+stack.append(0)
+for i in range(1, n):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
+
+
+print(*answer)
+    
     
