5fc67ffe940198e3222afa74f2f906033c1a4e9d
sdj3261 <63071967+sdj3261@users.noreply.github.com>
2024-01-17 00:19:06 +0000

    식당입구대기줄 문제 수정

--- 식당입구대기줄.py
+++ 식당입구대기줄.py
@@ -1,2 +1,21 @@
 from collections import deque
 n = int(input())
+q = deque()
+mx_stu = 0 
+stu_no = 0
+
+
+for i in range(n) :
+    info = list(map(int,input().split()))
+    if info[0] == 1 :
+        q.append(info[1])
+    elif info[0] == 2 :
+        if q :
+            q.popleft()
+    if mx_stu <= len(q) :
+        mx_stu = len(q)    
+        stu_no = q[-1]
+
+print(mx_stu, stu_no)
+
+    
