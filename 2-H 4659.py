0c058e60efe68cd3c0bba93065541eed2cf77a40
sdj3261 <tlsehdwo7@naver.com>
2025-05-31 14:19:28 +0900

    250531 algorithm study

--- 2-H 4659.py
+++ 2-H 4659.py
@@ -1,14 +1,45 @@
-n,c = map(int,input().split())
-arr = list(map(int,input().split()))
-
-m = dict()
-for i in arr :
-    if i in m :
-        m[i] += 1
+    # a,e,i,o,u 중 반드 시 하나 포함 체크
+    # 모음이 3개 자음이 3개 연속으로 오면 false
+    # 같은 글자 연속적으로 두번 오면 안되나 ee랑 oo만 허용
+def check1(data) :
+    if data.find('a') != -1 or data.find('e') != -1 or data.find('i') != -1 or data.find('o') != -1 or data.find('u') != -1 :
+        return True
     else :
-        m[i] = 1
+        return False
 
-sorted_arr = sorted(m.keys(),key=lambda x:-m[x])
-ret = []
-for i in sorted_arr :
-    print(m[i] * (str(i) + " "), end = "")
+def check2(data) :
+    moumCnt = 0
+    zaumCnt = 0
+    for i in range(len(data)) :
+        # 모음체크하는함수
+        if check1(data[i]) :
+            moumCnt += 1
+            zaumCnt = 0
+        else :
+            zaumCnt += 1
+            moumCnt = 0
+        if zaumCnt == 3 or moumCnt == 3 :
+            return False
+    return True
+
+def check3(data) :
+    for i in range(1,len(data)) :
+        pos = data[i-1]
+        if data[i] == 'o' or data[i] == 'e' :
+            continue
+        if pos == data[i] :
+            return False
+    return True
+
+
+while True :
+    data = input()
+    if data == 'end' :
+        break
+    print("<" + data + ">" + " is", end = ' ')
+
+    if check1(data) and check2(data) and check3(data) :
+        print("acceptable.")
+    else :
+        print("not acceptable.")
+
