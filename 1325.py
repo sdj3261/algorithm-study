632dbfcc469030d72e3523a47f0590607e07731e
sdj3261 <tlsehdwo7@naver.com>
2025-06-10 23:54:24 +0900

    250610 algorithm study 효율적인해킹

--- 1325.py
+++ 1325.py
@@ -1,2 +1,8 @@
-#효율적인해킹
-#
+from collections import deque
+n, m = map(int, input().split())
+board = [[0] * m for _ in range(n)]
+dy = [0,1,0,-1]
+dx = [1,0,-1,0]
+visited = [[False] * m for _ in range(n)]
+
+
