b9d8a6c092dc1512f78620a7501325f7818a53c7
sdj3261 <tlsehdwo7@naver.com>
2025-06-13 18:27:08 +0900

    250613 algorithm study 오큰수

--- 2-R 1068.py
+++ 2-R 1068.py
@@ -2,27 +2,33 @@ n = int(input())
 parent_node = list(map(int, input().split()))
 del_node = int(input())
 
-tree = {}
-for i in range(n):
-    if parent_node[i] == del_node or i == del_node :
-        continue
-    if parent_node[i] in tree:
-        tree[parent_node[i]].append(i)
-    else:
-        tree[parent_node[i]] = [i]
-print(tree)
-# 리프 노드 개수 세기
-leaf_count = 0
-if -1 in tree :
-    que = [-1]
-else :
-    que = []
-while que :
-    node = que.pop()
-    if node not in tree :
-        leaf_count += 1 
-    else :
-        que.extend(tree[node])
-    
+arr = [[] for _ in range(n)]
+visited = [False for _ in range(n)]
 
-print(leaf_count)
+# 트리 만들기 + 루트 찾기
+for i in range(n):
+    if parent_node[i] == -1:
+        root = i
+    else:
+        arr[parent_node[i]].append(i)
+
+# 🔥 삭제 노드를 부모의 자식 리스트에서 제거
+if parent_node[del_node] != -1:
+    arr[parent_node[del_node]].remove(del_node)
+
+ret = 0
+def dfs(x):
+    global ret
+    visited[x] = True
+    if len(arr[x]) == 0:
+        ret += 1
+        return
+    for j in arr[x]:
+        if not visited[j]:
+            dfs(j)
+
+if del_node == root:
+    print(0)
+else:
+    dfs(root)
+    print(ret)
