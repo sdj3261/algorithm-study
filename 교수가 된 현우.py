dcd8a22ce2251abe706f926d81ec569aede015fd
sdj3261 <tlsehdwo7@naver.com>
2025-06-14 17:22:08 +0900

    250614 algorithm study

--- 교수가 된 현우.py
+++ 교수가 된 현우.py
@@ -1,10 +1,13 @@
 t = int(input())
 
-for _ in range(t):
-    n = int(input())
-    count = 0
-    i = 5
-    while n // i: # n의 5의 약수를 구하자
-        count += n // i
-        i *= 5 # 5가 2번곱해졌을수도 있으므로 25 ,125 이런것도 다 구해주자
-    print(count) #
+
+for i in range(t) :
+    data = int(input())
+    fiveCount = 0
+    n = 5
+    divisor = 1
+    while n*divisor <= data :
+        fiveCount += data // (n * divisor)
+        divisor *= 5
+    print(fiveCount)
+
