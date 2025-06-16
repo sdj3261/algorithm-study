4de5377d00f72b45bddaae0bb6fceda2fc2fb7f2
sdj3261 <tlsehdwo7@naver.com>
2025-06-16 11:42:10 +0900

    250616 algorithm study

--- 2589.py
+++ 2589.py
@@ -1,34 +1,50 @@
 from collections import deque
+dy = [-1,0,1,0]
+dx = [0,1,0,-1]
 
 n, m = map(int, input().split())
-arr = [list(input()) for _ in range(n)]
+arr = [['0'] * m for _ in range(n)]
 
-dy = [0, 1, 0, -1]
-dx = [1, 0, -1, 0]
+visited = [[-1] * m for _ in range(n)]
+land_data = []
 
-def bfs(sy, sx):
+for i in range(n) :
+    data = input()
+    for j in range(m) :
+        arr[i][j] = data[j]
+        if arr[i][j] == 'L' :
+            land_data.append((i, j)) #땅 위치 저장
+q = deque()
+ret = 0
+
+def bfs(y,x) :
+    q.append((y,x))
+    visited[y][x] = 0
+    bfs_ret = 0
+    while q :
+        qy,qx = q.popleft()
+        for i in range(4) :
+            ny = dy[i] + qy
+            nx = dx[i] + qx
+
+            if ny < 0 or ny >= n or nx < 0 or nx >= m or visited[ny][nx] >= 0 :
+                continue
+            if arr[ny][nx] == 'W' : #물이면
+                continue
+            if arr[ny][nx] == 'L' :
+                q.append((ny,nx))
+                visited[ny][nx] = visited[qy][qx] + 1 #이동시 업데이트
+                bfs_ret = max(bfs_ret, visited[ny][nx])
+    return bfs_ret
+
+
+
+for y,x in land_data :
     visited = [[-1] * m for _ in range(n)]
-    q = deque()
-    q.append((sy, sx))
-    visited[sy][sx] = 0
-    max_dist = 0
+    #bfs 다른 L과의 최대 거리 저장
+    ret = max(ret,bfs(y,x))
 
-    while q:
-        y, x = q.popleft()
-        for d in range(4):
-            ny, nx = y + dy[d], x + dx[d]
-            if 0 <= ny < n and 0 <= nx < m:
-                if arr[ny][nx] == 'L' and visited[ny][nx] == -1:
-                    visited[ny][nx] = visited[y][x] + 1
-                    max_dist = max(max_dist, visited[ny][nx])
-                    q.append((ny, nx))
-    return max_dist
+print(ret)
 
-# 모든 'L'에서 BFS 돌려서 최댓값 찾기
-answer = 0
-for i in range(n):
-    for j in range(m):
-        if arr[i][j] == 'L':
-            answer = max(answer, bfs(i, j))
 
-print(answer)
+
