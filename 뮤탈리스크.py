f54dd1f30a6a5cf7b3053e3c07be24e8855e3e0f
sdj3261 <tlsehdwo7@naver.com>
2025-07-18 21:37:55 +0900

    250718 algorithm study

--- 뮤탈리스크.py
+++ 뮤탈리스크.py
@@ -1,51 +1,43 @@
+from collections import deque
+from itertools import permutations
 n = int(input())
-arr = list(map(int, input().split()))
-path = []
-ret_s = 10000000000
-# 순열 순서바꿔서도 계산해서 최소값 구하기
-# 순열 is S not in path
-# 콤비 is S
+hp = list(map(int, input().split()))
+visited = [[[False] * 61 for _ in range(61)] for _ in range(61)]
 
-def countAttack(path):
-    count = 0
-    newScv = []
-    for scv in path:
-        newScv.append(arr[scv])
-    while len(newScv) != 0:
-        dmg = 9
-        offset = 1
-        for i in range(len(newScv)):
+attacks = list(permutations([9,3,1]))
 
-            newScv[i] = newScv[i] - dmg // offset
-            offset *= 3
+while len(hp) < 3 :
+    hp.append(0)
+ret = 0
 
-        # 0 이하 제거
-        newScv = [hp for hp in newScv if hp > 0]
+def bfs() :
 
-        count += 1
-    return count
-#ddd
-# def permu() :
-#     if len(path) == len(arr) :
-#         print(*path)
-#         return
-#     for i in range(len(arr)):
-#         if arr[i] not in path :
-#             path.append(arr[i])
-#             permu()
-#             path.pop()
-# permu()
+    q = deque() #모든 hp가 0이되는 지점 bfs 로 찾기
+    q.append((hp[0], hp[1], hp[2], 0))
+    visited[hp[0]][hp[1]][hp[2]] = True
 
-def dfs():
-    global ret_s
-    if len(path) == len(arr):
-        ret_s = min(ret_s, countAttack(path))
-        return
-    for i in range(len(arr)):
-        if i not in path: # 이거 뺴면 중복순열 arr[i]가 들어왔는지 체크한다
-            path.append(i)
-            dfs()
-            path.pop()
+    while q :
+        data = q.popleft()
 
-dfs()
-print(ret_s)
+        x,y,z,ac = data
+        if x == 0 and y == 0 and z == 0 :
+            return ac
+
+        for attack in attacks :
+            d1,d2,d3 = attack
+            x1 = max(x - d1, 0)
+            y1 = max(y - d2, 0)
+            z1 = max(z - d3, 0)
+            if not visited[x1][y1][z1] :
+                visited[x1][y1][z1] = True
+                q.append((x1,y1,z1,ac+1))
+    return 0
+
+ac = bfs()
+print(ac)
+
+
+
+
+
+
