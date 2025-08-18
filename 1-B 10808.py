814041f226725376e7d7f14917bb2f231d36146c
sdj3261 <tlsehdwo7@naver.com>
2025-08-18 21:52:03 +0900

    250818 algorithm study

--- 1-B 10808.py
+++ 1-B 10808.py
@@ -1,8 +1,44 @@
-cnt = [0] * 26
-data = input()
+a = int(input())
+arr = [[0] * a for _ in range(a)]
+for i in range(a) :
+    data = input()
+    for j in range(a) :
+        arr[i][j] = int(data[j])
 
-for i in data :
-    cnt[ord(i)-ord('a')] += 1
+print(arr)
 
-for i in range(26) :
-    print(cnt[i], end=' ')
+def dfs(x,y, size) :
+    #사이즈 부터 체킹 해서
+    #만약 for문 돌아서 같지않다면 분할 진행
+    #분할하면 괄호 삽입
+    if size == 1 :
+        return str(arr[x][y])
+    half = size//2
+    isSame = True
+    #1사분면
+    for i in range(x, x+ size) :
+        if not isSame :
+            break
+        for j in range(y, y+size) :
+            if arr[x][y] != arr[i][j] :
+                isSame = False
+                break
+    if isSame:
+        return str(arr[x][y])
+    half = size // 2
+    return "(" \
+        + dfs(x, y, half) \
+        + dfs(x, y + half, half) \
+        + dfs(x + half, y, half) \
+        + dfs(x + half, y + half, half) \
+        + ")"
+
+print(dfs(0,0, a))
+
+
+
+
+
+
+
+
