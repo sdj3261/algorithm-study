20d3d7ae666eadeeb2f1db856e93b9bcd6494d8f
sdj3261 <tlsehdwo7@naver.com>
2025-08-19 23:09:05 +0900

    250818 algorithm study

--- 1-D 10988.py
+++ 1-D 10988.py
@@ -1,6 +1,17 @@
-data = input()
+a,b,c =  map(int,input().split())
+time = [0] * 101
+for i in range(3) :
+    at, lt = map(int,input().split())
+    for j in range(at, lt) :
+        time[j] += 1
 
-if data == data[::-1] :
-    print(1)
-else : 
-    print(0)
+ret = 0
+for i in range(len(time)) :
+    if time[i] == 1 :
+        ret += a
+    elif time[i] == 2 :
+        ret += (2 * b)
+    elif time[i] ==3 :
+        ret += (3 * c)
+print(ret)
+
