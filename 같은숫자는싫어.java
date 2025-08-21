4f0e8f507607be92729742d911fa58caf64842a4
sdj3261 <tlsehdwo7@naver.com>
2025-08-21 23:19:35 +0900

    250821 같은숫자는싫어 알고리즘 문제풀이

---
+++ 같은숫자는싫어.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] solution(int []arr) {
+                int idx = 0;
+                int len = arr.length;
+
+                ArrayDeque<Integer> stack = new ArrayDeque<>();
+                while (idx < len) {
+                    if(!stack.isEmpty() && arr[idx] == stack.getLast()) {
+                        idx++;
+                        continue;
+                    }
+                    stack.addLast(arr[idx++]); ;
+                }
+                return stack.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
