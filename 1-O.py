5a2ba1e66e377ec95a1e48edd1a621d2111f2995
sdj3261 <tlsehdwo7@naver.com>
2025-09-01 21:59:35 +0900

    250901 쿼드트리

--- 1-O.py
+++ 1-O.py
@@ -1,24 +1,44 @@
-from collections import deque
+a = int(input())
+arr = [[0] * a for _ in range(a)]
+for i in range(a) :
+    data = input()
+    for j in range(a) :
+        arr[i][j] = int(data[j])
 
-q = deque()
+print(arr)
 
-def find_answer(n):
-    visited = set()
-    q.append(('1', 1 % n))
-    visited.add(1 % n)
+def dfs(x,y, size) :
+    #사이즈 부터 체킹 해서
+    #만약 for문 돌아서 같지않다면 분할 진행
+    #분할하면 괄호 삽입
+    if size == 1 :
+        return str(arr[x][y])
+    half = size//2
+    isSame = True
+    #1사분면
+    for i in range(x, x+ size) :
+        if not isSame :
+            break
+        for j in range(y, y+size) :
+            if arr[x][y] != arr[i][j] :
+                isSame = False
+                break
+    if isSame:
+        return str(arr[x][y])
+    half = size // 2
+    return "(" \
+        + dfs(x, y, half) \
+        + dfs(x, y + half, half) \
+        + dfs(x + half, y, half) \
+        + dfs(x + half, y + half, half) \
+        + ")"
 
-    while q:
-        num_str, mod = q.popleft()
-        if mod == 0:
-            return len(num_str)
-        next_mod = (mod * 10 + 1) % n
-        visited.add(next_mod)
-        q.append((num_str + '1', next_mod))
-    
-    
+print(dfs(0,0, a))
 
-while True:
-    a = int(input())
-    if a == 0:
-        break
-    print(find_answer(a))
+
+
+
+
+
+
+
